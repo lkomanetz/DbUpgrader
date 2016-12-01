e1d85661157b7f94da6e47821f8f002554750fa2
Logan Komanetz <Logan Komanetz>
2016-12-01 17:57:45 -0600

    Forgot to add namespace.

---
+++ ScriptExecutor.Tests/Classes/MockScriptExecutor.cs
@@ -0,0 +1,44 @@
+﻿using BackingStore.Contracts;
+using Executioner.Contracts;
+using ScriptExecutor.Contracts;
+using ScriptLoader.Contracts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptExecutor.Tests.Classes {
+
+	public class MockScriptExecutor : IScriptExecutor {
+		private IBackingStore _backingStore;
+		private IScriptLoader _scriptLoader;
+
+		public MockScriptExecutor(IScriptLoader scriptLoader, IBackingStore backingStore) {
+			_scriptLoader = scriptLoader;
+			_backingStore = backingStore;
+			_scriptLoader.LoadDocuments(_backingStore);
+		}
+
+		public IList<Guid> CompletedDocuments { get { return _backingStore.GetCompletedDocumentIds(); } }
+
+		public void Execute() {
+			IList<ScriptDocument> docsToExecute = _backingStore.GetDocuments();
+			for (short i = 0; i < docsToExecute.Count; ++i) {
+				for (short j = 0; j < docsToExecute[i].Scripts.Count; ++j) {
+					Execute(docsToExecute[i].Scripts[j]);
+				}
+				docsToExecute[i].IsComplete = true;
+				_backingStore.Update(docsToExecute[i]);
+			}
+		}
+
+		private void Execute(Script script) {
+			script.IsComplete = true;
+			_backingStore.Update(script);
+		}
+
+	}
+
+}
