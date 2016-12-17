6c569d3f91056c56a3ba1cf90b66414d89dfab61
Logan Komanetz <Logan Komanetz>
2016-12-16 20:39:44 -0600

    Starting work on FileSystem backing store.

---
+++ BackingStore.FileSystem.Tests/FileSystemTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BackingStore.FileSystem.Tests {
+
+	[TestClass]
+	public class FileSystemTests {
+
+		[TestMethod]
+		public void TestMethod1() {
+		}
+
+	}
+
+}
---
+++ Executioner/BackingStore/FileSystemStore.cs
@@ -0,0 +1,62 @@
+﻿using Executioner.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Executioner.BackingStore {
+
+	public class FileSystemStore : IBackingStore {
+
+		public void Add(Script script) {
+			throw new NotImplementedException();
+		}
+
+		public void Add(ScriptDocument document) {
+			throw new NotImplementedException();
+		}
+
+		public void Clean() {
+			throw new NotImplementedException();
+		}
+
+		public bool Delete(Script script) {
+			throw new NotImplementedException();
+		}
+
+		public bool Delete(ScriptDocument document) {
+			throw new NotImplementedException();
+		}
+
+		public void Dispose() {
+			throw new NotImplementedException();
+		}
+
+		public IList<Guid> GetCompletedDocumentIds() {
+			throw new NotImplementedException();
+		}
+
+		public IList<Guid> GetCompletedScriptIdsFor(Guid documentId) {
+			throw new NotImplementedException();
+		}
+
+		public IList<ScriptDocument> GetDocuments(GetDocumentsRequest request = null) {
+			throw new NotImplementedException();
+		}
+
+		public IList<Script> GetScriptsFor(Guid documentId) {
+			throw new NotImplementedException();
+		}
+
+		public void Update(Script script) {
+			throw new NotImplementedException();
+		}
+
+		public void Update(ScriptDocument document) {
+			throw new NotImplementedException();
+		}
+
+	}
+
+}
