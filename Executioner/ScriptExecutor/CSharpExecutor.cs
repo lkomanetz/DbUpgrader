9bf5ed8c942459c38e790476c02e48dd47c8bc34
Logan Komanetz <Logan.Komanetz@networkedenergy.com>
2016-12-28 09:43:12 -0600

    Adds beginning of CSharp execution.

---
+++ Executioner/ScriptExecutor/CSharpExecutor.cs
@@ -0,0 +1,61 @@
+﻿using Executioner.Contracts;
+using Executioner.Contracts.ScriptExecutor;
+using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Executioner {
+
+	public class CSharpExecutor : IScriptExecutor {
+
+		public void Execute(string scriptText) {
+			CSharpCodeProvider provider = new CSharpCodeProvider();
+			CompilerParameters parameters = new CompilerParameters() {
+				GenerateInMemory = true,
+				GenerateExecutable = false
+			};
+
+			string csSource = GenerateSourceString(scriptText);
+			CompilerResults results = provider.CompileAssemblyFromSource(parameters, csSource);
+			CheckForErrors(results);
+
+			Assembly assembly = results.CompiledAssembly;
+			Type program = assembly.GetType(
+				$"{ScriptExecutorConstants.CS_NAMESPACE_NAME}.{ScriptExecutorConstants.CS_CLASS_NAME}"
+			);
+
+			MethodInfo method = program.GetMethod(ScriptExecutorConstants.CS_MAIN_METHOD);
+			method.Invoke(null, null);
+		}
+
+		private void CheckForErrors(CompilerResults results) {
+			if (results.Errors.HasErrors) {
+				StringBuilder sb = new StringBuilder();
+				
+				foreach (CompilerError error in results.Errors) {
+					sb.AppendLine($"Error ({error.ErrorNumber}): {error.ErrorText}");
+				}
+
+				throw new InvalidOperationException(sb.ToString());
+			}	
+		}
+
+		private string GenerateSourceString(string scriptText) {
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine("using System;");
+			sb.AppendLine($"namespace {ScriptExecutorConstants.CS_NAMESPACE_NAME} {{");
+			sb.AppendLine($"public class {ScriptExecutorConstants.CS_CLASS_NAME} {{");
+			sb.AppendLine($"public static void {ScriptExecutorConstants.CS_MAIN_METHOD}(){{");
+			sb.AppendLine(scriptText);
+			sb.AppendLine("}}}");
+
+			return sb.ToString();
+		}
+	}
+
+}
---
+++ ScriptExecutor.CSharp.Tests/CSharpExecutorTests.cs
@@ -0,0 +1,48 @@
+﻿using Executioner;
+using Executioner.Tests.Classes;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ScriptExecutor.CSharp.Tests {
+
+	[TestClass]
+	public class CSharpExecutorTests {
+
+		[TestMethod]
+		public void CSharp_NoExceptionsThrown() {
+			string code = @"Console.WriteLine(""Hello world!"");";
+
+			string[] scripts = new string[] {
+				$"<Script Id='{Guid.NewGuid()}' Executor='CSharpExecutor' Order='2016-12-28'>{code}</Script>"
+			};
+
+			ScriptExecutioner executioner = CreateExecutor(scripts);
+			executioner.Run();
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(InvalidOperationException), "Compile time error not found.")]
+		public void CSharp_CompilerErrorsFound() {
+			// This is missing the semi-colon
+			string code = @"Console.WriteLine(""Hello world!"")";
+
+			string[] scripts = new string[] {
+				$"<Script Id='{Guid.NewGuid()}' Executor='CSharpExecutor' Order='2016-12-28'>{code}</Script>"
+			};
+
+			ScriptExecutioner executioner = CreateExecutor(scripts);
+			executioner.Run();
+		}
+
+		private ScriptExecutioner CreateExecutor(string[] scripts) {
+			ScriptExecutioner executioner = new ScriptExecutioner(
+				new BaseMockLoader(scripts),
+				new MockLogger()
+			);
+			executioner.Add(new CSharpExecutor());
+
+			return executioner;
+		}
+	}
+
+}
