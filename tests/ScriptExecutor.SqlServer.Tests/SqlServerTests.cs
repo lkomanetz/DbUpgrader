28ac12c35c8151e3015a87ec0e6d74b1cc180481
Logan Komanetz <Logan.Komanetz@networkedenergy.com>
2017-03-09 10:30:51 -0600

    Adds new unit test around CDATA and cleans up the unit test a little bit.

--- tests/ScriptExecutor.SqlServer.Tests/SqlServerTests.cs
+++ tests/ScriptExecutor.SqlServer.Tests/SqlServerTests.cs
@@ -54,13 +54,30 @@ namespace ScriptExecutor.SqlServer.Tests {
 			Assert.True(ex is InvalidOperationException, "Empty SQL script attempted to run.");
 		}
 
-		// XML is handled by wrapping it around <![[CDATA]]>
 		[Fact]
-		public void ScriptExecutor_SqlServer_CDataHandledInSqlScript() {
+		public void ScriptExecutor_SqlServer_CDataHandledInSqlScriptSucceeds() {
+			string createTableScript = @"
+				IF OBJECT_ID('dbo.TestTable', 'U') IS NOT Null
+					DROP TABLE dbo.TestTable
+				CREATE TABLE dbo.TestTable(
+					TestXml XML NOT NULL
+				)
+			";
+			string completedlyCDataWrapped = @"
+				<![CDATA[
+					INSERT INTO TestTable
+					VALUES ('<root><child>Hello</child></root>')
+				]]>
+			";
+			string dropTableScript = "DROP TABLE TestTable";
+			string[] scripts = new string[] {
+				$"<Script Id='{Guid.NewGuid()}' Executor='SqlServerExecutor' Order='2017-03-08'>{createTableScript}</Script>",
+				$"<Script Id='{Guid.NewGuid()}' Executor='SqlServerExecutor' Order='2017-03-08:1'>{completedlyCDataWrapped}</Script>",
+				$"<Script Id='{Guid.NewGuid()}' Executor='SqlServerExecutor' Order='2017-03-08:2'>{dropTableScript}</Script>"
+			};
 			Exception ex = Record.Exception(() => {
-				IList<Assembly> assemblies = new List<Assembly>() { this.GetType().GetTypeInfo().Assembly };
 				ScriptExecutioner executioner = new ScriptExecutioner(
-					new AssemblyLoader(assemblies),
+					new BaseMockLoader(scripts),
 					new MockLogger()
 				);
 
@@ -72,6 +89,39 @@ namespace ScriptExecutor.SqlServer.Tests {
 				executioner.Run();
 			});
 
+			Assert.Null(ex);
+		}
+
+		[Fact]
+		public void ScriptExecutor_SqlServer_CDataInMiddleOfSqlSucceeds() {
+			string sqlScript = @"
+				CREATE TABLE dbo.AnotherTest (
+					RefId UNIQUEIDENTIFIER NOT NULL
+					, TestXml XML NOT NULL
+				)
+				DECLARE @id AS UNIQUEIDENTIFIER = NEWID();
+				DECLARE @xml as XML = <![CDATA['<root><child>Hello</child></root>']]>;
+
+				INSERT INTO dbo.AnotherTest
+				VALUES (@id, @xml)
+
+				DROP TABLE dbo.AnotherTest
+			";
+			string[] scripts = new string[] {
+				$"<Script Id='{Guid.NewGuid()}' Executor='SqlServerExecutor' Order='2017-03-08'>{sqlScript}</Script>"
+			};
+
+			Exception ex = Record.Exception(() => {
+				ScriptExecutioner executioner = new ScriptExecutioner(
+					new BaseMockLoader(scripts),
+					new MockLogger()
+				);
+				var executor = (SqlServerExecutor)executioner.ScriptExecutors
+					.Where(x => x.GetType() == typeof(SqlServerExecutor))
+					.Single();
+				executor.ConnectionString = connectionString;
+				executioner.Run();
+			});
 			Assert.Null(ex);
 		}
 
