139e1e0574aa961b3a4e7a68dd8653c611798360
Logan Komanetz <Logan.Komanetz@networkedenergy.com>
2017-03-30 12:34:12 -0500

    Forgot to add code changes.

---
+++ tests/ScriptExecutor.SqlServer.Tests/MockDataStore.cs
@@ -0,0 +1,27 @@
+
+using Executioner.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptExecutor.SqlServer.Tests {
+
+	public class MockDataStore : IDataStore {
+
+		public void Add(Script script) { }
+
+		public void Add(ScriptDocument document) { }
+
+		public void Clean() { }
+
+		public IList<Guid> GetCompletedDocumentIds() { return new List<Guid>(); }
+
+		public IList<Guid> GetCompletedScriptIdsFor(Guid documentId) { return new List<Guid>(); }
+
+		public void Update(Script script) { }
+
+	}
+
+}
